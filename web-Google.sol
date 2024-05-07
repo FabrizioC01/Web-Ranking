25def1e7503fbaf25cae88f1bf34eb1f12e12494
FabrizioC01 <fabriziocongiu1@gmail.com>
2024-05-07 09:16:55 +0000

    Aggiunti altri file

---
+++ web-Google.sol
@@ -0,0 +1,9 @@
+Number of nodes: 875713
+Number of dead-end nodes: 136259
+Number of valid arcs: 5105039
+Converged after 73 iterations
+Sum of ranks: 1.0000   (should be 1)
+Top 3 nodes:
+  41909 0.001015
+  597621 0.000969
+  537039 0.000934
