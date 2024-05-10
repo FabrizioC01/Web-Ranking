915037a1e37f26286fddad34e6014dd7e8b3418d
FabrizioC01 <fabriziocongiu1@gmail.com>
2024-05-10 16:34:39 +0000

    schema prod/cons funzionante

--- core/graph.c
+++ core/graph.c
@@ -2,37 +2,93 @@
 
 #define pos __LINE__,__FILE__
 
-typedef struct{
+//Strutture interne per il passaggio dei dati ai threads
+typedef struct coppia{
     int l;
     int r;
 } pair;
 
+//Strutture interne per il passaggio dei dati ai threads
 typedef struct dati{
     pair *buffer;
     int *pointer;
     pthread_mutex_t *mu;
     sem_t *f_slots;
     sem_t *b_slots;
-
+    graph *graph;
+    pthread_mutex_t *g_mutex;
+    int *count;
+    int *end_threads;
 } dati;
 
-void consumer_routine(void *data){
-    dati d = *(dati *)data;
+//Aggiunge in testa restituisce true se lo aggiunge, false altrimenti
+void push_inmap(inmap *input,int val){
+    inmap *head= input;
+    while(head!=NULL){
+        if(head->value==val) 
+        head=head->next;
+    }
+    
+    inmap *n_elem = malloc(sizeof(inmap));
+    n_elem->value=val;
+    n_elem->next=input;
+    input=n_elem;
     
 }
 
+//Funzione eseguita dai thread consumatori
+void *consumer_routine(void *data){
+    dati *d = (dati *)data;
+    pair coppia; //per salvare i valori temp
+    do{
+        xsem_wait(d->b_slots,pos);
+        xpthread_mutex_lock(d->mu,pos);
+        coppia.l=(d->buffer[*d->pointer % BUFF_SIZE]).l;
+        coppia.r=(d->buffer[*d->pointer % BUFF_SIZE]).r;
+        (*d->pointer)+=1;
+        fprintf(stderr,"\nLETTO %d -> %d\n",coppia.l,coppia.r);
+        xpthread_mutex_unlock(d->mu,pos);
+        xsem_post(d->f_slots,pos);
+
+        
+
+        if(coppia.l!=-1 && coppia.r!=-1 && coppia.l!=coppia.r){
+            //aggiungo al grafo
+            xpthread_mutex_lock(d->g_mutex,pos);
+            fprintf(stderr,"\nLETTO  %d -> %d\n",coppia.l,coppia.r);
+            (d->graph)->nodi+=1;
+            (d->graph)->out[coppia.l-1]+=1;
+            (*d->count)+=1;
+            inmap *list = (d->graph)->in[coppia.r-1];
+            push_inmap(list,coppia.r-1);
+            xpthread_mutex_unlock(d->g_mutex,pos);
+        }
+        if(coppia.l==-1 && coppia.r==-1){
+            break;
+        }
+    }while(true);
+
+    return NULL;
+}
+
 void start_graph(const int nodes,const int threads,const int iter,const int damp,const int m_err, FILE *infile){
+
     pair buffer[BUFF_SIZE];
-    ssize_t e;
-    int index=0,row=0,col=0,edges=0;
+    ssize_t e=0;
+    int cindex=0,pindex=0,row=0,col=0,edges=0;
     pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t graph_mutex = PTHREAD_MUTEX_INITIALIZER;
     sem_t free_slots;
     sem_t busy_slots;
 
+    xsem_init(&free_slots,0,BUFF_SIZE,pos);
+    xsem_init(&busy_slots,0,0,pos);
+
     graph g;
 
-    char *line;
-    ssize_t s;
+    char *line=NULL;
+    ssize_t s=0;
+
     //Lettura della prima linea
     while(true){
         e = getline(&line,&s,infile);
@@ -42,50 +98,71 @@ void start_graph(const int nodes,const int threads,const int iter,const int damp
             break;
         }
     } 
+
     if(row!=col) raise_error("Errore il file non contiene una matrice di adiacenza",pos);
 
     //Inizializzazione grafo vuoto
     g.nodi=0;
-    g.indici=NULL;
-    g.nodi=NULL;
-    g.out=NULL;
+    g.out= malloc(sizeof(int)*row);
+    g.in= malloc(sizeof(inmap)*row);
+    int count=0,end_threads=0;
+
+    for(int i=0;i<row;i++){
+        g.out[i]=0;
+        g.in[i]=NULL;
+    }
 
     dati d[N_THREADS];
     pthread_t t[N_THREADS];
     for(int i=0;i<N_THREADS;i++){
-        d[i].buffer = &buffer;
+        d[i].count=&count;
+        d[i].buffer = buffer;
         d[i].mu= &mutex;
-        d[i].pointer=&index;
+        d[i].pointer=&cindex;
         d[i].b_slots=&busy_slots;
         d[i].f_slots=&free_slots;
-        pthread_create(&t[i],NULL,&consumer_routine,&d[i]);
+        d[i].graph= &g;
+        d[i].g_mutex= &graph_mutex;
+        xpthread_create(&t[i],NULL,consumer_routine,&d[i],pos);
     }
 
     int from,to;
 
+    //Produttore che passa i dati e i valori di terminazione
     while(true){
         e = getline(&line,&s,infile);
         if(e==EOF){
             for(int i=0;i<N_THREADS;i++){
-                sem_wait(&free_slots);
-                pthread_mutex_lock(&mutex);
-                buffer[index].l=-1;
-                buffer[index].r=-1;
-                index++;
-                pthread_mutex_unlock(&mutex);
-                sem_post(&busy_slots);
+                xsem_wait(&free_slots,pos);
+                xpthread_mutex_lock(&mutex,pos);
+                buffer[pindex % BUFF_SIZE].l=-1;
+                buffer[pindex % BUFF_SIZE].r=-1;
+                pindex++;
+                xpthread_mutex_unlock(&mutex,pos);
+                xsem_post(&busy_slots,pos);
             } 
-            break;
+            break; 
         }
         if(line[0]!='%'){
             sscanf(line,"%d %d",&from,&to);
-            sem_wait(&free_slots);
-            pthread_mutex_lock(&mutex);
-            buffer[index].l=from;
-            buffer[index].r=to;
-            index++;
-            pthread_mutex_unlock(&mutex);
-            sem_post(&busy_slots);
+            xsem_wait(&free_slots,pos);
+            xpthread_mutex_lock(&mutex,pos);
+            buffer[pindex % BUFF_SIZE].l=from;
+            buffer[pindex % BUFF_SIZE].r=to;
+            pindex++;
+            xpthread_mutex_unlock(&mutex,pos);
+            xsem_post(&busy_slots,pos);
         }
     }
+
+
+    for(int i=0;i<N_THREADS;i++){
+        fprintf(stderr,"\nIl thread %d ha terminato",i);
+        xpthread_join(t[i],NULL,pos);
+    }
+    fprintf(stderr,"\n%d\n",count);
+    xsem_destroy(&free_slots,pos);
+    xsem_destroy(&busy_slots,pos);
+    xpthread_mutex_destroy(&mutex,pos);
+    xpthread_mutex_destroy(&graph_mutex,pos);
 }
--- error/error.c
+++ error/error.c
@@ -1,10 +1,21 @@
 #include "error.h"
 
+#define Buflen 100
+void xperror(int en, char *msg) {
+  char buf[Buflen];
+  
+  char *errmsg = strerror_r(en, buf, Buflen);
+  if(msg!=NULL)
+    fprintf(stderr,"%s: %s\n",msg, errmsg);
+  else
+    fprintf(stderr,"%s\n",errmsg);
+}
+
 void raise_error(const char *mex,int line, char *file){
     if(errno==0){
-        fprintf(stderr,"\n\n==%d== %s (%s:%d)",getpid(),mex,file,line);
+        fprintf(stderr,"\n\n== %d == Linea: %d, File: %s - %s\n",getpid(),line,file,mex);
     }else{
-        fprintf(stderr,"\n\n==%d== %s (%s:%d) | Errno: %s",getpid(),mex,file,line,strerror(errno));
+        fprintf(stderr,"\n\n== %d == Linea: %d, File: %s - %s\n %s",getpid(),line,file,mex,strerror(errno));
     }
     exit(1);
 }
@@ -13,8 +24,111 @@ FILE *xfopen(const char *path, const char *mode, int linea, char *file) {
   FILE *f = fopen(path,mode);
   if(f==NULL) {
     perror("Errore apertura file");
-    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    fprintf(stderr,"\n\n== %d == Linea: %d, File: %s\n",getpid(),linea,file);
     exit(1);
   }
   return f;
+}
+
+int xsem_init(sem_t *sem, int pshared, unsigned int value, int linea, char *file) {
+  int e = sem_init(sem,pshared,value);
+  if(e !=0) {
+    perror("Errore sem_init"); 
+    fprintf(stderr,"\n\n== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    exit(1);
+  }
+  return e;
+}
+
+int xsem_post(sem_t *sem, int linea, char *file) {
+  int e = sem_post(sem);
+  if(e !=0) {
+    perror("Errore sem_post"); 
+    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    exit(1);
+  }
+  return e;
+}
+
+int xsem_wait(sem_t *sem, int linea, char *file) {
+  int e = sem_wait(sem);
+  if(e !=0) {
+    perror("Errore sem_wait"); 
+    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    exit(1);
+  }
+  return e;
+}
+
+int xsem_destroy(sem_t *sem, int linea, char *file) {
+  int e = sem_destroy(sem);
+  if(e !=0) {
+    perror("Errore sem_destroy"); 
+    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    exit(1);
+  }
+  return e;
+}
+
+int xpthread_create(pthread_t *thread, const pthread_attr_t *attr,
+                          void *(*start_routine) (void *), void *arg, int linea, char *file) {
+  int e = pthread_create(thread, attr, start_routine, arg);
+  if (e!=0) {
+    xperror(e, "Errore pthread_create");
+    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    pthread_exit(NULL);
+  }
+  return e;                       
+}
+                          
+int xpthread_join(pthread_t thread, void **retval, int linea, char *file) {
+  int e = pthread_join(thread, retval);
+  if (e!=0) {
+    xperror(e, "Errore pthread_join");
+    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    pthread_exit(NULL);
+  }
+  return e;
+}
+
+
+
+int xpthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr_t *restrict attr, int linea, char *file) {
+  int e = pthread_mutex_init(mutex, attr);
+  if (e!=0) {
+    xperror(e, "Errore pthread_mutex_init");
+    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    pthread_exit(NULL);
+  }  
+  return e;
+}
+
+int xpthread_mutex_destroy(pthread_mutex_t *mutex, int linea, char *file) {
+  int e = pthread_mutex_destroy(mutex);
+  if (e!=0) {
+    xperror(e, "Errore pthread_mutex_destroy");
+    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    pthread_exit(NULL);
+  }
+  return e;
+}
+
+int xpthread_mutex_lock(pthread_mutex_t *mutex, int linea, char *file) {
+  int e = pthread_mutex_lock(mutex);
+  if (e!=0) {
+    xperror(e, "Errore pthread_mutex_lock");
+    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    pthread_exit(NULL);
+  }
+  return e;
+}
+
+int xpthread_mutex_unlock(pthread_mutex_t *mutex, int linea, char *file) {
+  int e = pthread_mutex_unlock(mutex);
+  if (e!=0) {
+    xperror(e, "Errore pthread_mutex_unlock");
+    fprintf(stderr,"== %d == Linea: %d, File: %s\n",getpid(),linea,file);
+    pthread_exit(NULL);
+  }
+  return e;
 }
